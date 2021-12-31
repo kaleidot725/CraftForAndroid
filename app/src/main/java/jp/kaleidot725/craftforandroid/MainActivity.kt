3205df2382af5df9b46da38ab725dfb00823b5b9
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2021-12-31 18:37:17 +0900

    feat: add load craft web

--- app/src/main/java/jp/kaleidot725/craftforandroid/MainActivity.kt
+++ app/src/main/java/jp/kaleidot725/craftforandroid/MainActivity.kt
@@ -1,18 +1,46 @@
 package jp.kaleidot725.craftforandroid
 
+import android.content.Intent
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.KeyEvent.*
 import im.delight.android.webview.AdvancedWebView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var webView: AdvancedWebView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val webView = findViewById<AdvancedWebView>(R.id.web_view)
+        webView = findViewById<AdvancedWebView>(R.id.web_view)
         webView.setDesktopMode(true)
         webView.settings.builtInZoomControls = false
         webView.settings.displayZoomControls = false
         webView.loadUrl("https://docs.craft.do")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        webView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        webView.onPause()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        webView.onActivityResult(requestCode, resultCode, intent)
+    }
+
+    override fun onBackPressed() {
+        if (!webView.onBackPressed()) {
+            return
+        }
+        super.onBackPressed()
     }
 }
